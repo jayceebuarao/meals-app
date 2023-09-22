7ac613e4c0e3392c4b0394bbfe179c4a87178f3a
Jaycee Buarao <jayceebuarao@gmail.com>
2023-09-21 19:00:20 -0700

    refactor: move navigation management to categories_screen.dart

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
 import 'package:meals_app/widgets/categories_grid_item.dart';
+import 'package:meals_app/screens/meals_screen.dart';
+import 'package:meals_app/models/category.dart';
+import 'package:meals_app/models/meal.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context, Category category) {
+    List<Meal> categorizedMeal = dummyMeals
+        .where((element) => element.categories.contains(category.id))
+        .toList();
+
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) => MealsScreen(
+        title: category.title,
+        meals: categorizedMeal,
+      ),
+    ));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +36,12 @@ class CategoriesScreen extends StatelessWidget {
             mainAxisSpacing: 12),
         children: [
           for (final category in availableCategories)
-            CategoriesGridItem(category: category)
+            CategoriesGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context, category);
+              },
+            )
         ],
       ),
     );
