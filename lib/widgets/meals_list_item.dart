28e8466b864c2c90c76d94bfccf7692172ba914a
Jaycee Buarao <jayceebuarao@gmail.com>
2023-09-21 20:34:43 -0700

    style: refactor item with card and stack widget

--- lib/widgets/meals_list_item.dart
+++ lib/widgets/meals_list_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class MealsListItem extends StatelessWidget {
   const MealsListItem({super.key, required this.meal});
@@ -8,23 +9,38 @@ class MealsListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {},
-      child: Container(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
+    return Card(
+      color: Theme.of(context).primaryColorLight,
+      margin: const EdgeInsets.all(8),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      clipBehavior: Clip.hardEdge,
+      child: InkWell(
+        onTap: () {},
+        child: Stack(
           children: [
-            SizedBox(
+            FadeInImage(
+              placeholder: MemoryImage(kTransparentImage),
+              image: NetworkImage(meal.imageUrl),
+              height: 280,
               width: double.infinity,
-              child: Image.network(
-                meal.imageUrl,
-                height: 200,
-                fit: BoxFit.fitWidth,
+              fit: BoxFit.cover,
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                color: Colors.black45,
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  meal.title,
+                  style: const TextStyle(color: Colors.white, fontSize: 18),
+                  softWrap: true,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                ),
               ),
             ),
-            const SizedBox(height: 16),
-            Text(meal.title, style: const TextStyle(color: Colors.white)),
           ],
         ),
       ),
