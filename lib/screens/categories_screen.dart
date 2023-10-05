d47510317d8a985a9c2ee5e74b28046ef6567f45
Jaycee Buarao <jayceebuarao@gmail.com>
2023-10-04 19:37:04 -0700

    feat: add ease in anim to grid list

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -5,7 +5,7 @@ import 'package:meals_app/screens/meals_screen.dart';
 import 'package:meals_app/models/category.dart';
 import 'package:meals_app/models/meal.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({
     super.key,
     required this.availableMeals,
@@ -13,12 +13,40 @@ class CategoriesScreen extends StatelessWidget {
 
   final List<Meal> availableMeals;
 
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController
+      _animationController; //late keyword means variable will eventually have a value
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+      lowerBound: 0,
+      upperBound: 1,
+    );
+
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   /// @desc when category is selected, take in type Category parameter of selected category
   /// and sort through all meals under that category and store to List of type Meal 'categorizedMeal'
   ///
   /// Navigate to MealsScreen with sorted list of meals
   void _selectCategory(BuildContext context, Category category) {
-    List<Meal> categorizedMeal = availableMeals
+    List<Meal> categorizedMeal = widget.availableMeals
         .where((element) => element.categories.contains(category.id))
         .toList();
 
@@ -32,22 +60,33 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      padding: const EdgeInsets.all(24),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 1.4,
-          crossAxisSpacing: 16,
-          mainAxisSpacing: 16),
-      children: [
-        for (final category in availableCategories)
-          CategoriesGridItem(
-            category: category,
-            onSelectCategory: () {
-              _selectCategory(context, category);
-            },
-          )
-      ],
+    return AnimatedBuilder(
+      animation: _animationController,
+      child: GridView(
+        padding: const EdgeInsets.all(24),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 1.4,
+            crossAxisSpacing: 16,
+            mainAxisSpacing: 16),
+        children: [
+          for (final category in availableCategories)
+            CategoriesGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context, category);
+              },
+            )
+        ],
+      ),
+      builder: (context, child) => SlideTransition(
+        position: Tween(
+          begin: const Offset(0, 0.2),
+          end: const Offset(0, 0),
+        ).animate(CurvedAnimation(
+            parent: _animationController, curve: Curves.easeInOut)),
+        child: child,
+      ),
     );
   }
 }
